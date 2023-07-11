5d4e38aa6e8c35bf458f1995e22903210d471a17
snnaenu <455342265@qq.com>
2023-07-11 09:15:41 +0800

    mobile

---
+++ examples/vue3-theme-mobile/src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <button @click="add" style="margin-top: 50px; font-size: 30px">
+    我是主题：点我给插件加1
+  </button>
+  <div id="freelog-single"></div>
+</template>
+<script setup>
+let app = null;
+// 加载自身依赖的插件
+const mountSubWidget = async () => {
+  const subData = await window.freelogApp.getSubDep();
+  subData.subDep.some(async (sub, index) => {
+    if (index === 0) return true;
+    app = await window.freelogApp.mountWidget({
+      widget: sub, // 必传，子插件数据
+      container: document.getElementById("freelog-single"), // 必传，自定义一个让插件挂载的div容器
+      topExhibitData: subData, // 必传，最外层展品数据（子孙插件都需要用）
+      config: {}, // 子插件配置数据，需要另外获取作品上的配置数据
+      seq: null, // 如果要用多个同样的子插件需要传递序号，可以考虑与其余节点插件避免相同的序号, 注意用户数据是根据插件id+序号保存的。
+      widget_entry: "https://localhost:8002", // 本地url，dev模式下，可以使用本地url调试子插件
+    });
+    // 使用此函数可以保证在插件加载完成后 再执行
+    app.mountPromise.then(() => {
+      // 卸载之后重新加载
+      // app.unmount.then(() => {
+      //   app.mount.then(() => {
+      //   })
+      // })
+    });
+  });
+};
+const add = () => {
+  app.getApi().changeMe();
+};
+
+// 加载展品类型的插件s
+const mountExhibitWidget = async () => {
+  const res = await window.freelogApp.getExhibitListByPaging({
+    articleResourceTypes: "插件",
+    isLoadVersionProperty: 1,
+  });
+  const widgets = res.data.data.dataList;
+  console.log(widgets);
+  widgets.some(async (widget, index) => {
+    if (index === 1) { 
+      return true;
+    }
+    // widget.exhibitId = widget.exhibitId + '111'
+    app = await window.freelogApp.mountWidget({
+      widget: widget, // 必传，子插件数据
+      container: document.getElementById("freelog-single"), // 必传，自定义一个让插件挂载的div容器
+      topExhibitData: null, // 必传，最外层展品数据（子孙插件都需要用）
+      config: {}, // 子插件配置数据，需要另外获取作品上的配置数据
+      seq: null, // 如果要用多个同样的子插件需要传递序号，可以考虑与其余节点插件避免相同的序号, 注意用户数据是根据插件id+序号保存的。
+      widget_entry: "https://localhost:8002", // 本地url，dev模式下，可以使用本地url调试子插件
+    });
+  });
+};
+mountExhibitWidget();
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ examples/vue3-theme-mobile/vue.config.js
@@ -0,0 +1,61 @@
+const { defineConfig } = require('@vue/cli-service')
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+const path = require('path');
+const { name } = require('./package');
+const webpackPlugin = require('webpack-mkcert')
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+const port = 8001;
+module.exports = defineConfig(async () => {
+  const https = await webpackPlugin.default({
+    force: true,
+    source: 'coding',
+    hosts: ['localhost', '127.0.0.1']
+  })
+  console.log(https)
+  return {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    transpileDependencies: true,
+    outputDir: 'dist',
+    assetsDir: 'static',
+    filenameHashing: true,
+    devServer: {
+      https: {
+        ...https
+      },
+      hot: true,
+      port,
+      historyApiFallback: true,
+      allowedHosts: 'all',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    },
+    // 自定义webpack配置
+    configureWebpack: {
+      resolve: {
+        alias: {
+          '@': resolve('src'),
+        },
+      },
+      output: {
+        // 把子应用打包成 umd 库格式
+        library: `${name}-[name]`,
+        libraryTarget: 'umd',
+        chunkLoadingGlobal: `webpackJsonp_${name}`,
+      },
+    },
+  }
+})
