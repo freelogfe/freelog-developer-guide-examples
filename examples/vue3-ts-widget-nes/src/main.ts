fe3d33e3433c1ffe75efb380dcc47a20ae70fe80
snnaenu <455342265@qq.com>
2023-07-12 15:29:40 +0800

    nes

---
+++ examples/vue3-ts-widget-nes/src/main.ts
@@ -0,0 +1,95 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import "./public-path";
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import routes from "./router";
+import { createPinia } from "pinia";
+import { createRouter, createWebHistory } from "vue-router";
+import { useCounterStore } from "./stores/counter";
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import "./assets/css/index.scss"
+
+const freelogApp = window.freelogApp;
+
+let pinia: any = null;
+
+// createApp(App).use(store).use(router).mount("#app")
+let router: any = null;
+let instance: any = null;
+
+function render(props: any = {}) {
+  const { container } = props;
+  router = createRouter({
+    history: createWebHistory(window.__POWERED_BY_FREELOG__ ? "/widget" : "/"),
+    routes,
+  });
+  instance = createApp(App);
+  pinia = createPinia();
+  instance.use(router);
+  instance.use(pinia).use(Antd);
+
+  instance.mount(container ? container.querySelector("#app") : "#app");
+  // instance.config.globalProperties.$message = message;
+
+  if (props?.registerApi) {
+    // 暴露api给父插件或主题
+    props.registerApi({
+      changeMe: () => {
+        const store = useCounterStore();
+        store.increment();
+      },
+    });
+  }
+}
+
+if (!window.__POWERED_BY_FREELOG__) {
+  render();
+}
+
+export async function bootstrap() {
+  console.log("%c ", "color: green;", "vue3.0 app bootstraped");
+}
+
+function storeTest(props: any) {
+  if (props.onGlobalStateChange) {
+    props.onGlobalStateChange(
+      (value: any, prev: any) =>
+        console.log(`[插件 - ${props.name}]:`, value, prev),
+      true
+    );
+  }
+  setTimeout(() => {
+    props.setGlobalState({
+      ignore: props.name + "111",
+      user: {
+        name: props.name + "111",
+      },
+    });
+  }, 2500);
+  if (props.setGlobalState) {
+    props.setGlobalState({
+      ignore: props.name,
+      user: {
+        name: props.name,
+      },
+    });
+  }
+}
+
+export async function mount(props: any) {
+  storeTest(props);
+  render(props);
+  instance.config.globalProperties.$onGlobalStateChange =
+    props.onGlobalStateChange;
+  instance.config.globalProperties.$setGlobalState = props.setGlobalState;
+}
+
+export async function unmount() {
+  instance.unmount();
+  instance._container.innerHTML = "";
+  instance = null;
+  router = null;
+  pinia = null;
+}
---
+++ examples/vue3-ts-widget-nes/src/views/HomeView.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="home w-100x h-100x flex-column">
+    <div class="flex-row px-200 pt-40 space-between">
+      <a-button type="primary" class="mr-20" @click="closeVoice">关闭声音</a-button>
+      <a-button type="primary" class="mr-20" @click="setKeyVisible = true"
+        >设置按键</a-button
+      >
+    </div>
+    <nes-vue
+      width="800"
+      height="700"
+      :p1="p1Keys"
+      :p2="p2Keys"
+      url="https://taiyuuki.github.io/nes-vue/Super Mario Bros (JU).nes"
+    />
+    <UserInfo
+      v-if="setKeyVisible"
+      :visible="setKeyVisible"
+      @cancel="setKeyVisible = false"
+      :p1Keys="p1Keys"
+      :p2Keys="p2Keys"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+// import { Controller } from "jsnes";
+import UserInfo from "./UserInfo.vue";
+import { NesVue } from "nes-vue";
+import { ref } from "vue";
+import { storeToRefs } from "pinia";
+import { freelogApp } from "freelog-runtime";
+import { message } from "ant-design-vue";
+
+const setKeyVisible = ref(false);
+const p1 = {
+  UP: "KeyW",
+  DOWN: "KeyS",
+  LEFT: "KeyA",
+  RIGHT: "KeyD",
+  A: "KeyK",
+  B: "KeyJ",
+  C: "KeyI",
+  D: "KeyU",
+  SELECT: "Digit2",
+  START: "Digit1",
+};
+const p2 = {
+  UP: "ArrowUp",
+  DOWN: "ArrowDown",
+  LEFT: "ArrowLeft",
+  RIGHT: "ArrowRight",
+  A: "Numpad2",
+  B: "Numpad1",
+  C: "Numpad5",
+  D: "Numpad4",
+};
+const p1Keys = ref({ ...p1 });
+const p2Keys = ref({ ...p2 });
+
+function upstart() {
+  document.dispatchEvent(new KeyboardEvent("keydown", { code: "KeyW" }));
+}
+function closeVoice() {
+  message.error("This is an error message");
+
+  // document.dispatchEvent(new KeyboardEvent("keyup", { code: "KeyW" }));
+}
+
+ 
+// the increment action can just be destructured
+</script>
