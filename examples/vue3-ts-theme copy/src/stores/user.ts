fd5fa42116b50d5ac31b80bff32a6e7a4ab460b7
snnaenu <455342265@qq.com>
2023-08-21 10:26:19 +0800

    ui

--- examples/vue3-ts-theme copy/src/stores/user.ts
+++
@@ -1,9 +0,0 @@
-import { defineStore } from 'pinia'
-export const userStore = defineStore('user', {
-  state: () => ({ userInfo: null as any }), 
-  actions: {
-    setUserInfo(data:any) {
-      this.userInfo = data
-    },
-  },
-})
---
+++ examples/vue3-ts-widget-nes/README.md
@@ -0,0 +1,4 @@
+# 文档地址
+
+https://widget-docs.freelog.com/
+
