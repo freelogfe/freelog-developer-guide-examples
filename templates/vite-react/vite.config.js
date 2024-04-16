a0a570ecce9de36252d7075dd922e6e34c9a13c9
snnaenu <455342265@qq.com>
2024-04-16 16:10:07 +0800

    d

--- templates/vite-react/vite.config.js
+++ templates/vite-react/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [basicSsl(),react()],
+  server: {
+    port: 8880,
+  },
+  resolve: {
+    // 配置路径别名
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 })
--- templates/vite-vue-ts/vite.config.ts
+++ templates/vite-vue-ts/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [basicSsl(),vue()],
+  server: {
+    port: 8880,
+  },
+  resolve: {
+    // 配置路径别名
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 })
